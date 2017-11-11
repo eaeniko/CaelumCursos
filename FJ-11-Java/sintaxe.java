be91da73058d3db2ae1b861cf07f6e8f19decc75
Nikolai Cinotti <agencianiko@hotmail.com>
2017-11-11 14:20:54 -0200

    comentarios

--- FJ-11-Java/sintaxe.java
+++ FJ-11-Java/sintaxe.java
@@ -1,24 +1,19 @@
 class Sintaxe {
 	public static void main(String[] args) {
 		
+		//soma 
 		for (int i = 150; i <= 300; i++) {
 			// System.out.println(i);
 			
 		}
 
+
+		// somar o anterior até mil
 			int soma = 0;
 		for (int b= 1; b <= 1000; b++) {
 			soma = soma + b; // soma = soma +b
 		}
 			System.out.println(soma);
-	
-		// for (int c= 0; c <= 100 ; c+=3) {
-			
-		// 	System.out.println(c);
-			
-		// }
-
-
 
 	}
 

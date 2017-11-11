ae11fd598f64277de10d9cbddf97e18a79c5e197
Nikolai Cinotti <agencianiko@hotmail.com>
2017-11-11 14:31:27 -0200

    outra opção de multiplos com módulo %

--- FJ-11-Java/Multiplo.java
+++ FJ-11-Java/Multiplo.java
@@ -7,3 +7,15 @@ class Multiplo {
 		}
 	}
 }
+
+
+
+// public static void main(String[] args) {
+// 	for (int i = 1; i<=100 ; i++) {
+
+// 		if(i % 3 == 0) {
+// 			System.out.println(i);
+// 		}
+		
+// 	}
+// }0

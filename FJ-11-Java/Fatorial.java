a72a48e8075db1b92abc69d1bac8c3d5551b4e96
Nikolai Cinotti <agencianiko@hotmail.com>
2017-11-11 14:46:57 -0200

    refatorando

--- FJ-11-Java/Fatorial.java
+++ FJ-11-Java/Fatorial.java
@@ -1,10 +1,10 @@
 class Fatorial {
 	public static void main(String[] args) {
 		
-		int fatorial = 1;
-		for (int i = 1; i <= 10 ; i++) {
+		long fatorial = 1;
+		for (int i = 1; i <= 40 ; i++) {
 		
-			fatorial =   i * fatorial;
+			fatorial *= i;
 			System.out.println(fatorial);
 
 		}		

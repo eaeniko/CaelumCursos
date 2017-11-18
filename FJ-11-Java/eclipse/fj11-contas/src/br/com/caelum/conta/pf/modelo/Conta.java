f41893f2d3c35a0b6c5b20926ddcb39a9933c3b1
Nikolai Cinotti <agencianiko@hotmail.com>
2017-11-18 16:54:17 -0200

    Criando um javadoc

--- FJ-11-Java/eclipse/fj11-contas/src/br/com/caelum/conta/pf/modelo/Conta.java
+++ FJ-11-Java/eclipse/fj11-contas/src/br/com/caelum/conta/pf/modelo/Conta.java
@@ -1,5 +1,10 @@
 package br.com.caelum.conta.pf.modelo;
 
+/**
+ * Classe que gerencia as contas do sistema
+ * @author Nikolai
+ * 
+ */
 public class Conta {
 	private String titular;
 	private String agencia;
@@ -30,9 +35,17 @@ public class Conta {
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
+	/**
+	 * método que devolve o saldo da conta
+	 * @return o saldo
+	 */
 	public double getSaldo() {
 		return saldo;
 	}
+	/**
+	 * Método que incrementa o saldo da conta
+	 * @param valor o valor a ser depositado
+	 */
 	public void deposita(double valor) {
 		// TODO Auto-generated method stub
 		this.saldo += valor;

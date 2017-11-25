d3fd4127d2b981409f069557a643dff975c052cb
Nikolai Cinotti <agencianiko@hotmail.com>
2017-11-25 14:00:46 -0200

    Aplicando os conceitos de Herança e Polimorfismo

--- FJ-11-Java/eclipse/fj11-contas/src/br/com/caelum/contas/ManipuladorDeContas.java
+++ FJ-11-Java/eclipse/fj11-contas/src/br/com/caelum/contas/ManipuladorDeContas.java
@@ -1,16 +1,25 @@
 package br.com.caelum.contas;
 
 import br.com.caelum.contas.modelo.Conta;
+import br.com.caelum.contas.modelo.ContaCorrente;
+import br.com.caelum.contas.modelo.ContaPoupanca;
 import br.com.caelum.javafx.api.util.Evento;
 
-public class ManipuladorDeContas {
+public class ManipuladorDeContas extends Conta {
 	
 	private Conta conta;
 	public void criaConta(Evento evento) {
-		conta = new Conta();
-		conta.setTitular("Batman");
-		conta.setAgencia("123-4");
-		conta.setNumero(345);
+		
+		String tipo = evento.getSelecionadoNoRadio("tipo");
+		if (tipo.equals("Conta Corrente")) {
+			this.conta = new ContaCorrente();
+		} else if (tipo.equals("Conta Poupança")) {
+			this.conta = new ContaPoupanca();
+		}
+		this.conta = new Conta();
+		this.conta.setTitular(evento.getString("titular"));
+		this.conta.setAgencia(evento.getString("agencia"));
+		this.conta.setNumero(evento.getInt("numero"));
 	}
 	
 	public void deposita(Evento evento) {
---
+++ FJ-11-Java/eclipse/fj11-contas/src/br/com/caelum/contas/modelo/ContaPoupanca.java
@@ -0,0 +1,7 @@
+package br.com.caelum.contas.modelo;
+
+public class ContaPoupanca extends Conta {
+	public static void main(String[] args) {
+		
+	}
+}

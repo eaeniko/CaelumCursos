1e4b7a020ac18f9699404c398214379bcdd933c3
Nikolai Cinotti <agencianiko@hotmail.com>
2017-11-25 09:25:13 -0200

    Importanto um Jar externo e testando 1ª Janela Gráfica

--- FJ-11-Java/eclipse/fj11-contas/src/br/com/caelum/conta/main/TestaConta.java
+++ FJ-11-Java/eclipse/fj11-contas/src/br/com/caelum/conta/main/TestaConta.java
@@ -1,15 +1,17 @@
 package br.com.caelum.conta.main;
 import br.com.caelum.conta.pf.modelo.Conta;
 
+
 public class TestaConta {
 
 	public static void main(String[] args) {
 
 		Conta conta = new Conta();
 		conta.setTitular("Nikolai");
-		conta.deposita(100.5);
+		conta.deposita(600.5);
 		conta.saca(400);
 
 		System.out.println(conta.getTitular());
+		System.out.println(conta.getSaldo());
 	}
 }
---
+++ FJ-11-Java/eclipse/fj11-contas/src/br/com/caelum/conta/main/TestaJar.java
@@ -0,0 +1,9 @@
+package br.com.caelum.conta.main;
+
+import br.com.caelum.javafx.api.main.OlaMundo;
+
+public class TestaJar {
+	public static void main(String[] args) {
+		OlaMundo.main(args);
+	}
+}

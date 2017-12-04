4f3366439b39a1e77446540e8510e3ad93f01100
Nikolai Cinotti <agencianiko@hotmail.com>
2017-12-04 14:18:00 -0200

    Collections
    Utilizando Array List, foreach, size, get, add e remove

---
+++ FJ-11-Java/AluraJava/gerenciador-de-cursos/src/br/com/aluracursos/TestandoListas.java
@@ -0,0 +1,27 @@
+package br.com.aluracursos;
+
+import java.util.ArrayList;
+
+ public class TestandoListas {
+	
+	public static void main(String[] args) {
+		String aula1 = "Conhecendo mais sobre listas";
+		String aula2 = "Modelando a classe Aula";
+		String aula3 = "Trabalhando com Cursos e Sets";
+		
+		ArrayList<String> aulas = new ArrayList<>();
+		aulas.add(aula1);
+		aulas.add(aula2);
+		aulas.add(aula3);
+		
+		System.out.println(aulas);
+		
+		aulas.remove(0);
+		
+		System.out.println(aulas);
+		
+		for (String aula : aulas) {
+			System.out.println("Aula: " + aula);
+		}
+	}
+}

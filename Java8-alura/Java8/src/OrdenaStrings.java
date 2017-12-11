86925bf41a4285b871f45d5a6f85f8025d6b608b
Nikolai Cinotti <agencianiko@hotmail.com>
2017-12-11 14:39:08 -0200

    Metodos defaults
    foreach, consumer, accept, comparator

---
+++ Java8-alura/Java8/src/OrdenaStrings.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class OrdenaStrings {
+	public static void main(String[] args) {
+
+		List<String> palavras = new ArrayList<>();
+		palavras.add("alura online");
+		palavras.add("casa do código");
+		palavras.add("caelum");
+
+		Comparator<String> comparador = new ComparadorPorTamanho();
+		// Collections.sort(palavras, comparador);
+		palavras.sort(comparador);
+		System.out.println(palavras);
+
+
+		Consumer<String> consumidor = new ImprimeNaLinha();
+		palavras.forEach(consumidor);
+	}
+
+}
+
+class ImprimeNaLinha implements Consumer<String> {
+
+	@Override
+	public void accept(String s) {
+		System.out.println(s);
+	}
+
+}
+
+class ComparadorPorTamanho implements Comparator<String> {
+	public int compare(String s1, String s2) {
+		if (s1.length() < s2.length())
+			return -1;
+		if (s1.length() > s2.length())
+			return 1;
+		return 0;
+	}
+}
